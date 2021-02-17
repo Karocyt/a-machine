2f9920483ac1822b56228da5eca21f2b22fb819c
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2021-02-17 15:29:41 +0100

    error added when defining transitions using unknown chars

--- src/Machine.hs
+++ src/Machine.hs
@@ -9,6 +9,10 @@ module Machine where
 import Data.Aeson.Types (Parser, Object, Value)
 import qualified Data.HashMap.Strict as HM
 import Data.Aeson (FromJSON(..), parseJSON, (.:), withObject)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Control.Monad (join)
 
 -- for Map (String, Char) Transition
 import Data.Map (Map)
@@ -19,7 +23,7 @@ import Data.Typeable
 instance (Typeable a, Typeable b) => Show (a->b) where
   show _ = show $ typeOf (undefined :: a -> b)
 
-type Tape = String -- might be better with a Array-like monad ? Thinking about infinite Tape
+type Tape = String -- might be better with a Array-like Map or similar ? Thinking about infinite Tape
 type Move = Int
 -- Map added to parameters for now as I can't see how to properly curry it
 -- type Transition = Map (String, Char) Transition -> State -> Either String State
@@ -28,7 +32,7 @@ newtype Transition = Transition { runTransition :: Map (String, Char) Transition
 -- Static Machine type
 data Machine = Machine {
     name :: String,
-    alphabet :: [Char],
+    alphabet :: Set Char,
     blank :: Char,
     finals :: [String],
     transitions :: Map (String, Char) Transition,
@@ -61,23 +65,27 @@ genericTransition :: Char -> Move -> String -> Map (String, Char) Transition -> 
 genericTransition = error "Not implemented yet"
 -- TO DO
 
-buildTransition :: String -> Object -> Parser TransitionStruct
-buildTransition name fields = do -- Parser
+buildTransition :: String -> Object -> Set Char -> Parser TransitionStruct
+buildTransition name fields mAlphabet = do -- Parser
         tmpRead <- fields .: "read"
+        if Set.member tmpRead mAlphabet
+            then pure True else fail $ "Function '"++ name ++ "' is able to read '" ++ tmpRead:"' which is not in the machine alphabet."
         tmpToState <- fields .: "to_state"
         tmpWrite <- fields .: "write"
+        if Set.member tmpWrite mAlphabet
+            then pure True else fail $ "On '" ++ tmpRead:"', function '"++ name ++ "' is able to write '" ++ tmpWrite:"' which is not in the machine alphabet."
         tmpAction <- fields .: "action"
         tmpMove <- case (stringToMove tmpAction) of
-            Left s -> error s
+            Left s -> fail s
             Right m -> return m
         return $ TransitionStruct name tmpRead tmpToState tmpWrite tmpMove
 
-parseTransitions :: Value -> Parser [Parser TransitionStruct]
-parseTransitions raw =
+parseTransitions :: Value -> Set Char -> Parser [Parser TransitionStruct]
+parseTransitions raw mAlphabet =
     -- Conversion function + composition operator
     foldl (\globalAcc (name, linesArray :: [Object]) ->
         (foldl (\nameAcc lineObject ->
-            (buildTransition name lineObject):nameAcc) [] linesArray) ++ globalAcc) []
+            (buildTransition name lineObject mAlphabet):nameAcc) [] linesArray) ++ globalAcc) []
         .
     -- Turn the HashMap with random name into a list of pairs (name, [objects]) and apply (<$>) operator
     HM.toList <$>
@@ -93,16 +101,17 @@ instance FromJSON Machine where
     parseJSON = withObject "machine" $ \o -> do -- in Parser (kinda Either String Value)
         mName <- o .: "name"
         alphabetStrings <- o .: "alphabet" :: Parser [String]
-        let mAlphabet = foldl (\acc curr_elem -> (head curr_elem):acc) [] alphabetStrings
-        if (onlyUnique mAlphabet)
+        let lAlphabet = foldl (\acc curr_elem -> (head curr_elem):acc) [] alphabetStrings
+        if (onlyUnique lAlphabet)
             then pure True else fail "Duplicates found in alphabet"
         mBlank <- o .: "blank"
-        if (elem mBlank mAlphabet)
+        if (elem mBlank lAlphabet)
             then pure True else fail "Blank char missing in alphabet"
+        let mAlphabet = Set.fromList lAlphabet
         mFinals <- o .: "finals"
         mInitial <- o .: "initial" :: Parser String
         transitionsListObject <- o .: "transitions" -- > Parser Object
-        transitionsListParsed <- parseTransitions transitionsListObject -- [Parser TransitionStruct] <- (Parser Object -> (Parser [Parser TransitionStruct])) 
+        transitionsListParsed <- parseTransitions transitionsListObject mAlphabet -- [Parser TransitionStruct] <- (Parser Object -> (Parser [Parser TransitionStruct])) 
         transitions <- sequence $ transitionsListParsed -- [t] <- [Parser t] -> Parser [t]
         -- can't include the Map in curryied genericTransition... ?!
         let mTransitions = foldl (\acc currT -> Map.insert (tName currT, tRead currT) (Transition (genericTransition (tWrite currT) (tMove currT) (tToState currT))) acc) Map.empty transitions
